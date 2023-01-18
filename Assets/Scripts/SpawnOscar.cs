324540760972a489e08e4019070af549a2e175ce
orodrigzz <oscarrod18503@gmail.com>
2023-01-18 19:09:51 +0100

    Score y Highscore

--- Assets/Scripts/SpawnOscar.cs
+++ Assets/Scripts/SpawnOscar.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnOscar : MonoBehaviour
 {
+    //Spawner
     public GameObject Circle;
     private int num= 0;
 
     public float x;
     public float y;
     Vector2 randomPos;
+
+    //Highscore
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = score.ToString();
+        highscoreText.text = highscore.ToString();
+    }
 
     void Update()
     {
@@ -20,12 +36,24 @@ public class SpawnOscar : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
            num++;
+
+           //Highscore
+           AddPoint();
         }
 
         if (num == 5)
         {
             Instantiate(Circle, randomPos, Quaternion.identity);
             num = 0;
+        }
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        scoreText.text = score.ToString();
+        if (highscore < score) {
+            PlayerPrefs.SetInt("highscore", score);
         }
     }
 }
